61c8cabb65b85faec74d30dcebe07cfd09109f1a
jdrodriguezrui <jdrodriguezrui@unal.edu.co>
2019-08-30 23:56:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,41 @@
                             <th>No.</th>
                             <th>Task</th>                            
                         </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>Mark</td>
+                                <td><a href="" class="btn btn-success" data-target="#myModal" data-toggle="modal">Edit</a></td>
+                                <td><a href="" class="btn btn-danger">Delete</a></td>
+                            </tr>
+                        </tbody>
                     </table>
-                </div>                
+                </div>
+                
+                <div id="myModal" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Edit task</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="">Task name</label>
+                                    <input type="text" required name="task">
+                                </div>
+                                <input type="submit" name="send" value="Send" class="btn btn-success">
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>    
 
