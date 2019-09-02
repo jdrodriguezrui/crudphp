f58ed2793d0efd5e94a86f987762f4fa4118070d
jdrodriguezrui <jdrodriguezrui@unal.edu.co>
2019-09-02 18:59:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include 'db.php'; ?>
 <html>
     <head>
-        <title>Crud App Test</title>
+        <title>Crud App with DB</title>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
