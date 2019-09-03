9446c443e315acd44d2d811a80ac7305ee495221
jdrodriguezrui <jdrodriguezrui@unal.edu.co>
2019-09-02 21:29:01 -0500

    Update add.php

--- add.php
+++ add.php
@@ -1,1 +1,14 @@
-<?php echo"ok";?>
+<?php
+include 'db.php';
+
+if(isset($_POST['send'])){
+    $name= $_POST['task'];
+    $sql = "insert into TASKS (name) values ('$name')";
+    $val = $db->query($sql);
+
+    if($val){
+        header('location: index.php')
+    }
+}
+
+?>
